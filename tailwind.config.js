8f20ee23d3e4f6cafc1cd52a0396a9511b39ab27
Sunita-Gurau <sunitagurau.1999@gmail.com>
2024-12-01 12:12:00 +0545

    update Tailwind CSS config with custom utilities, theme extensions, and tw-elements plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,55 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './index.html',
+    './src/**/*.{vue,js,ts,jsx,tsx}',
+    './node_modules/tw-elements/js/**/*.js',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          1: '#A5A3AE4D',
+          2: '#F3F4F8',
+          3: '#23272E',
+          4: '#8B909A',
+          5: '#DBDADE',
+        },
+        blue: {
+          1: '#7367F0',
+          2: '#0F60FF',
+        },
+      },
+      fontSize: {
+        '13px': [
+          '0.8125rem',
+          {
+            lineHeight: '1.125rem',
+          },
+        ],
+        '32px': ['2rem'],
+      },
+      width: {
+        66: '17rem',
+        1: '0.0625rem',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      addUtilities({
+        '.poke-scrollbar::-webkit-scrollbar': {
+          width: '0.125rem',
+        },
+        '.poke-scrollbar::-webkit-scrollbar-thumb': {
+          backgroundColor: '#CACACA',
+        },
+        '.poke-scrollbar::-webkit-scrollbar-track': {
+          backgroundColor: '#EEEEEE',
+        },
+        '.poke-transition': { transition: 'all 500ms ease-in-out' },
+      })
+    },
+    require('tw-elements/plugin.cjs'),
+  ],
 }
